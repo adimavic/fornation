d456fb862dc222779c580256ff1906ea885b2f45
Aditya kale <119602309+adimavic@users.noreply.github.com>
2025-10-29 12:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -262,4 +262,5 @@ def handle_connect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True, allow_unsafe_werkzeug=True)
+
